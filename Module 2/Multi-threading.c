2f47194010b3b3f297e28321e3d5dfa8a8371391
S Vimal <161236095+vimal21343@users.noreply.github.com>
2026-06-07 12:41:49 +0530

    Update Multi-threading.c

--- Module 2/Multi-threading.c
+++ Module 2/Multi-threading.c
@@ -1,54 +1,86 @@
-#include<stdio.h>
-#include<pthread.h>
-#include<unistd.h>
-#include<time.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <time.h>
 
 int N;
 
-int isPrime(int n){
-    if(n<2) return 0;
-    for(int i=2; i*i<=n; i++)
-        if(n%i==0) return 0;
+/* Function to check whether a number is prime */
+int isPrime(int num)
+{
+    if(num < 2)
+        return 0;
+
+    for(int i = 2; i * i <= num; i++)
+    {
+        if(num % i == 0)
+            return 0;
+    }
     return 1;
 }
 
-void* sum_Prime(void* arg){
-    int count=0, sum=0;
-    for(int i=2; count<N; i++)
-        if(isPrime(i)){ sum+=i; count++; }
-    printf("Sum of first %d primes = %d\n", N, sum);
+/* Thread A */
+void *primeSum(void *arg)
+{
+    int count = 0, num = 2;
+    long long sum = 0;
+
+    while(count < N)
+    {
+        if(isPrime(num))
+        {
+            sum += num;
+            count++;
+        }
+        num++;
+    }
+
+    printf("Sum of first %d prime numbers = %lld\n", N, sum);
     pthread_exit(NULL);
 }
 
-void* thread_1(void* arg){
-    time_t t = time(NULL);
-    while(time(NULL)-t < 100){
+/* Thread B */
+void *thread1(void *arg)
+{
+    time_t start = time(NULL);
+
+    while(time(NULL) - start < 100)
+    {
         printf("Thread 1 running\n");
         sleep(2);
     }
+
     pthread_exit(NULL);
 }
 
-void* thread_2(void* arg){
-    time_t t = time(NULL);
-    while(time(NULL)-t < 100){
+/* Thread C */
+void *thread2(void *arg)
+{
+    time_t start = time(NULL);
+
+    while(time(NULL) - start < 100)
+    {
         printf("Thread 2 running\n");
         sleep(3);
     }
+
     pthread_exit(NULL);
 }
 
-void main(){
+int main()
+{
     pthread_t t1, t2, t3;
-    printf("Enter N: "); scanf("%d", &N);
 
-    pthread_create(&t1, NULL, sum_Prime, NULL);
-    pthread_create(&t2, NULL, thread_1, NULL);
-    pthread_create(&t3, NULL, thread_2, NULL);
+    printf("Enter N: ");
+    scanf("%d", &N);
+
+    pthread_create(&t1, NULL, primeSum, NULL);
+    pthread_create(&t2, NULL, thread1, NULL);
+    pthread_create(&t3, NULL, thread2, NULL);
 
     pthread_join(t1, NULL);
     pthread_join(t2, NULL);
     pthread_join(t3, NULL);
 
-    printf("All threads done!\n");
+    return 0;
 }
